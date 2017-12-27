dfda7e0dac8bafadd5012ebcf65a73260aac0104
fanwei <fanerwei222@163.com>
2017-12-27 11:56:27 +0800

    ...

--- src/main/java/com/ulyne/fan/FanApplication.java
+++ src/main/java/com/ulyne/fan/FanApplication.java
@@ -1,11 +1,13 @@
 package com.ulyne.fan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.ulyne")
+@ComponentScan(basePackages = "com.ulyne") //扫描该文件夹下面的所有控制器什么的
+@MapperScan("com.ulyne.mybatis.mapper")//对mybatis mapper文件夹进行扫描
 public class FanApplication {
 
 	public static void main(String[] args) {
